28ca5776d3bf5686a1290a8a6a0184408e58f9bb
LAB LAB <you@example.com>
2023-07-24 14:05:18 +0200

    Modification de lodging, affichage

--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -40,36 +40,45 @@ export default function Lodging() {
 						images={currentLodging.pictures}
 					/>
 					<div className='Lodging_content'>
-						<h1 className='Lodging_content_infos'>{currentLodging.title}</h1>
-						<p>{currentLodging.location}</p>
-						<div>
-							{currentLodging.tags.map((tag, index) => (
-								<button key={index}>{tag}</button>
-							))}
-						</div>
-						<div className='Lodging_content_host'>
-							<span>{currentLodging.host.name.split(' ')[0]}</span>
-							<span>{currentLodging.host.name.split(' ')[1]}</span>
-							<img src={currentLodging.host.picture} alt="host of this lodging" />
-						</div>
-						<div className='Lodging_content_host_stars'>
-							{[...Array(5)].map((star, index) => {
-								const ratingValue = index + 1;
-								return (
-									<img
-										key={index}
-										src={ratingValue <= currentLodging.rating ? redStar : greyStar}
-										alt="star"
-									/>
-								)
-							})}
-						</div>
-						<div className="Lodging_collapse">
-							<div className="Lodging_collapse_item">
-								<Collapse title={'Description'} content={currentLodging.description} />
+						<div className='duobox'>
+							<div className='duobox-left'>
+								<h1 className='Lodging_content_infos'>{currentLodging.title}</h1>
+								<p>{currentLodging.location}</p>
+								<div>
+									{currentLodging.tags.map((tag, index) => (
+										<button className="Lodging_content_infos_tagbutton" key={index}>{tag}</button>
+										
+									))}
+								</div>
 							</div>
-							<div className="Lodging_collapse_item">
-								<Collapse title={'Equipments'} content={currentLodging.equipments}/>
+							<div className='duobox-right'>
+								<div className='Lodging_content_host'>
+									<span>{currentLodging.host.name.split(' ')[0]}</span>
+									<span>{currentLodging.host.name.split(' ')[1]}</span>
+									<img src={currentLodging.host.picture} alt="host of this lodging" />
+								</div>
+								<div className='Lodging_content_host_stars'>
+									{[...Array(5)].map((star, index) => {
+										const ratingValue = index + 1;
+										return (
+											<img
+												key={index}
+												src={ratingValue <= currentLodging.rating ? redStar : greyStar}
+												alt="star"
+											/>
+										)
+									})}
+								</div>
+							</div>
+						</div>
+						<div className="Lodging_collapse duobox">
+							<div className="Lodging_collapse_item duobox-left">
+								<Collapse title="Description" content={currentLodging.description} />
+							</div>
+							<div className="Lodging_collapse_item duobox-right">
+								<Collapse title="Equipments" content={currentLodging.equipments && currentLodging.equipments.map((equipment, index) => (
+									<li key={index}>{equipment}</li>
+								))} />
 							</div>
 						</div>
 					</div>
