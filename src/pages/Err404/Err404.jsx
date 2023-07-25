54b97da704965782e68dee70b1d81ab698cdb670
LAB LAB <you@example.com>
2023-07-25 12:39:43 +0200

    Améliorations responsive + fichiers mis à jours

--- src/pages/Err404/Err404.jsx
+++ src/pages/Err404/Err404.jsx
@@ -7,7 +7,7 @@ export default function Err404() {
 		<div className='err404'>
 			<div className="err404_infos">
 				<h1 className='err404_infos_title'>404</h1>
-				<p className='err404_infos_content'>La page que vous recherchez n'existe pas.</p>
+				<p className='err404_infos_content'>Oups! La page que vous demandez n'existe pas.</p>
 			</div>
 			<Link className='err404_infos_return' to='/'>Retourner sur la page d'accueil</Link>
 		</div>
